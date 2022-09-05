3d64cbda0a64e80f3be5a3caf5a3a8965e112932
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-09-05 17:40:52 +0900

    [수정] server.js: GraphQL SDL 추가

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+/**
+ * Alias type Query -> Root
+ * type Root 안의 모든 것들은 Rest API에서 URL을 만드는 것과 같다.
+ */
+const typeDefs = gql`
+    schema {
+        query: Root
+    } 
 
-server.listen().then(({url}) => {
+    type Root {
+        text: String
+        hello: String 
+    } 
+`; // SDL(Schema Definition Language)를 미리 정의를 해줘야 ApolloServer Error가 안남 
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
 })

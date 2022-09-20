34816d09f200a0cc8373cfd0de72b3b8828a3b7d
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-09-20 23:27:48 +0900

    [수정] server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ const resolvers = {
  * Alias type Query -> Root
  * type Root 안의 모든 것들은 Rest API에서 URL을 만드는 것과 같다.
  * 
- * REST에서 POST를 담당하는 스페셜 type. -> Mutation 
+ * REST에서 POST를 담당하는 스페셜 type. -> Mutation
  */
 const typeDefs = gql`
     schema {
@@ -93,7 +93,7 @@ const typeDefs = gql`
         postTweet(text: String!, userId: ID!): Tweet!
         deleteTweet(id: ID!): Boolean!
     }
-`; // SDL(Schema Definition Language)를 미리 정의를 해줘야 ApolloServer Error가 안남 
+`; // SDL(Schema Definition Language)를 미리 정의를 해줘야 ApolloServer Error가 안남.
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
